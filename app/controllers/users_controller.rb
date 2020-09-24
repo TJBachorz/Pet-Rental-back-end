3174dab8ceaa82c69106114041c782f3a866ced9
TJBachorz <TJBachorz@gmail.com>
2020-09-24 10:56:15 -0600

    adds auth to backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create, :login]
 
     def index
         @users = User.all
@@ -11,9 +12,32 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(
-            name: params[:name]
-        )
-        redirect_to "https://pet-renter.web.app/user/user.html?user_id=#{@user.id}"
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to "https://pet-renter.web.app/user/user.html?user_id=#{@user.id}"
+        else
+            render json: {user: @user.errors.full_messages}
+        end    
+    end
+
+    def login
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            render json: {user: @user, token: @token}
+        else
+            render json: {message: "Invalid credentials."}
+        end
+    end
+
+    def profile
+        render json: @user
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :rentals
     has_many :pets, through: :rentals
 
